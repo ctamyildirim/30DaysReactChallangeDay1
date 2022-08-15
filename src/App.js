2940f66902e72003a9c7b4669804af21130d4eb2
Cihan Tamyıldırım <ctamyildirim@gmail.com>
2022-08-15 23:48:08 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header'
+import Userlist from './components/Userlist'
+
+
+function App() {
+  return (
+    <div className="App">
+        <Header></Header>
+        <Userlist></Userlist>
+    </div>
+  );
+}
+
+export default App;
